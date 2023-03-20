53fcc12d9af550c42fe362156899faee8c8d643a
StevanFreeborn <=>
2023-03-19 19:05:23 -0500

    fix: give delete command proper name

--- src/OnspringCLI/Commands/Attachments/DeleteCommand.cs
+++ src/OnspringCLI/Commands/Attachments/DeleteCommand.cs
@@ -2,7 +2,7 @@ namespace OnspringCLI.Commands.Attachments;
 
 public class DeleteCommand : Command
 {
-  public DeleteCommand() : base("download", "Download attachments")
+  public DeleteCommand() : base("delete", "Delete attachments")
   {
     AddCommand(
       new Delete.BulkCommand()
