286607c9b02a750d205988c8e309e769b5cb81ef
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-03-23 17:47:50 -0500

    tests: update test coverage script

--- tests/OnspringCLI.Tests/Scripts/test-coverage.ps1
+++ tests/OnspringCLI.Tests/Scripts/test-coverage.ps1
@@ -1,16 +1,16 @@
 $RootPath = Split-Path $PSScriptRoot -Parent
-$CoveragePath = "$RootPath\TestResults\coveragereport"
+$TestResults = "$RootPath\TestResults"
 
-if (Test-Path $CoveragePath) 
+if (Test-Path $TestResults) 
 { 
-  Remove-Item $RootPath\TestResults\coveragereport -Recurse -Force; 
+  Remove-Item $TestResults -Recurse -Force; 
 }
 
 dotnet test --environment ENVIRONMENT=testing --collect:"XPlat Code Coverage;Include=[OnspringCLI]*;ExcludeByFile=**/program.cs"
 
 reportgenerator `
--reports:$RootPath\TestResults\*\coverage.cobertura.xml `
--targetdir:$RootPath\TestResults\coveragereport `
+-reports:$TestResults\*\coverage.cobertura.xml `
+-targetdir:$TestResults\coveragereport `
 -reporttypes:Html_Dark;
 
 $TestsProjectDirectory = Split-Path (Split-Path $PSScriptRoot -parent) -Leaf
