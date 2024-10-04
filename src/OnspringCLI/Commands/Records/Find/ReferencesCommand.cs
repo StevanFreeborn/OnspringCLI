2b37a524fb1dacdf80c9fe7c44f3bbea889ba4fe
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-10-04 14:01:22 -0500

    feat: add options for references command

--- src/OnspringCLI/Commands/Records/Find/ReferencesCommand.cs
+++ src/OnspringCLI/Commands/Records/Find/ReferencesCommand.cs
@@ -4,5 +4,33 @@ public class ReferencesCommand : Command
 {
   public ReferencesCommand() : base("references", "Locate references to records")
   {
+    AddOption(
+      new Option<int>(
+        aliases: ["--app-id", "-a"],
+        description: "The app id to search for references in."
+      )
+      {
+        IsRequired = true
+      }
+    );
+
+    AddOption(
+      new Option<List<int>>(
+        aliases: ["--record-ids", "-r"],
+        description: "A comma separated list of record ids to find references for.",
+        parseArgument: result => result.ParseToIntegerList()
+      )
+      {
+        IsRequired = true
+      }
+    );
+
+    AddOption(
+      new Option<string>(
+        aliases: ["--output-directory", "-o"],
+        description: "The name of the directory to write the report to.",
+        getDefaultValue: () => "output"
+      )
+    );
   }
 }
--- tests/OnspringCLI.Tests/UnitTests/Commands/Records/Find/ReferencesCommandTests.cs
+++ tests/OnspringCLI.Tests/UnitTests/Commands/Records/Find/ReferencesCommandTests.cs
@@ -11,4 +11,38 @@ public class ReferencesCommandTests
     referencesCommand.Name.Should().Be("references");
     referencesCommand.Description.Should().Be("Locate references to records");
   }
+
+  [Fact]
+  public void ReferencesCommand_WhenCalled_ItShouldHaveARequiredAppIdOption()
+  {
+    var referencesCommand = new ReferencesCommand();
+
+    referencesCommand.Options
+      .FirstOrDefault(x => x.Name == "app-id")
+      .Should()
+      .NotBeNull().And.BeOfType<Option<int>>();
+  }
+
+  [Fact]
+  public void ReferencesCommand_WhenCalled_ItShouldHaveARequiredRecordIdsOption()
+  {
+    var referencesCommand = new ReferencesCommand();
+
+    referencesCommand.Options
+      .FirstOrDefault(x => x.Name == "record-ids")
+      .Should()
+      .NotBeNull().And.BeOfType<Option<List<int>>>();
+  }
+
+  [Fact]
+  public void ReferencesCommand_WhenCalled_ItShouldHaveAnOptionalOutputDirectoryOption()
+  {
+    var referencesCommand = new ReferencesCommand();
+
+    var outputDirectoryOption = referencesCommand.Options.FirstOrDefault(o => o.Name == "output-directory");
+
+    outputDirectoryOption.Should().NotBeNull();
+    outputDirectoryOption.Should().BeOfType<Option<string>>();
+    outputDirectoryOption!.IsRequired.Should().BeFalse();
+  }
 }
